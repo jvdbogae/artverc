c406690cdbaf6faaadf65654e860b777601e0762
vuong-buihv <37221772+vuong-buihv@users.noreply.github.com>
2018-11-23 22:13:59 +0100

    Added the volume urls

--- scrapers/kenya_gazette/kenya_gazette/spiders/gazette_notice.py
+++ scrapers/kenya_gazette/kenya_gazette/spiders/gazette_notice.py
@@ -2,16 +2,23 @@ import scrapy
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 from bs4 import BeautifulSoup
+import requests
 
 class GazetteNoticeSpider(scrapy.Spider):
     name = "Gazette Notices"
-
+    
+    def get_urls():
+        year_urls = ['http://kenyalaw.org/kenya_gazette/gazette/year/'+str(year) for year in range(2005, 2019)]
+        urls = []
+        for url in year_urls:
+            r = requests.get(url)
+            bsObj = BeautifulSoup(r.text)
+            urls.extend([link.get('href') for link in bsObj.find_all('a')])
+        urls = list(set(urls))
+        return [url for url in urls if 'http://kenyalaw.org/kenya_gazette/gazette/volume/' in url]
+ 
     def start_requests(self):
-        urls = [
-            'http://kenyalaw.org/kenya_gazette/gazette/volume/MTYzNA--/Vol.CXX-No.14',
-            'http://kenyalaw.org/kenya_gazette/gazette/volume/OTIw/Vol.%20CXV%20-%20No.%2017',
-        ]
-        for url in urls:
+        for url in get_urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
